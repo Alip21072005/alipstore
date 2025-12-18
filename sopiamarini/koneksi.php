f77a7c4515fd177a2ed9a1b25eb85b3f7d908a64
Alip Maulana <muhamadalipmaulana3@gmail.com>
2025-12-18 15:29:56 +0700

    Update database connection settings in koneksi.php for production environment

--- sopiamarini/koneksi.php
+++ sopiamarini/koneksi.php
@@ -1,7 +1,7 @@
 <?php
-$host     = "localhost";
-$user     = "root";
-$pass    = "";
+$host     = "202.10.34.87";
+$user     = "alip";
+$pass    = "Alip210725_";
 $dbname    = "sopiamarini";
 $conn     = mysqli_connect($host, $user, $pass, $dbname)
     or die("Gagal terkoneksi ke database");
