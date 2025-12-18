7c1dcd19a698066e25360efa4df4ab2ca5eb6ed5
Alip Maulana <muhamadalipmaulana3@gmail.com>
2025-12-18 09:58:07 +0700

    Update database connection credentials in koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $host     = "localhost";
-$user     = "root";
-$pass    = "password";
+$user     = "root@202.10.34.87";
+$pass    = "Alip210725_";
 $dbname    = "dbkedai";
 $conn     = mysqli_connect($host, $user, $pass, $dbname)
     or die("Gagal terkoneksi ke database");
