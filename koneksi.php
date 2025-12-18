c35dcb53f8ee84374a79bf5a46338608f301c5ae
Alip Maulana <muhamadalipmaulana3@gmail.com>
2025-12-18 09:42:02 +0700

    Update database connection credentials and add SQL dump for dbkedai

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $host     = "localhost";
-$user     = "root";
-$pass    = "";
+$user     = "alip";
+$pass    = "Alip210725_";
 $dbname    = "dbkedai";
 $conn     = mysqli_connect($host, $user, $pass, $dbname)
     or die("Gagal terkoneksi ke database");
