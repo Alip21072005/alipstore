350c2b51f79878f686cccaa450f6d74f8d31f960
Alip Maulana <muhamadalipmaulana3@gmail.com>
2025-12-20 14:33:28 +0700

    afwe

--- fella/dbkedai.sql
+++ fella/dbkedai.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 20 Nov 2025 pada 03.24
--- Versi server: 10.4.24-MariaDB
--- Versi PHP: 8.1.6
+-- Waktu pembuatan: 20 Des 2025 pada 05.10
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,15 @@ SET time_zone = "+00:00";
 CREATE TABLE `kategori` (
   `idkategori` int(3) NOT NULL,
   `namakategori` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`idkategori`, `namakategori`) VALUES
+(1, 'Makanan'),
+(2, 'Minuman');
 
 -- --------------------------------------------------------
 
@@ -46,7 +54,21 @@ CREATE TABLE `produk` (
   `deskripsi` text NOT NULL,
   `status` tinyint(1) NOT NULL,
   `idkategori` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `produk`
+--
+
+INSERT INTO `produk` (`idproduk`, `namaproduk`, `harga`, `gambar`, `deskripsi`, `status`, `idkategori`) VALUES
+(1, 'Donat', 5000, 'produk1766113487.jpg', '<p>Donat labu dan kentang dengan ribuan variasi yang nikmat</p>\r\n', 1, 1),
+(2, 'cake coklat', 25000, 'produk1766113797.jpg', '<p>cake coklat yang nyoklat banget</p>\r\n', 1, 1),
+(3, 'tiramisu', 30000, 'produk1766114342.jpg', '<p>tiramisu yang ngopi banget</p>\r\n', 1, 1),
+(4, 'Macchiato', 25000, 'produk1766114557.jpg', '<p>caramel macchiato yang ngaramel banget</p>\r\n', 1, 2),
+(5, 'Dopion', 18000, 'produk1766114902.jpg', '<p>kopi hitam tanpa gula yang sanggat cocock untuk anda yang sakit hati biar sakit lambung sekalian</p>\r\n', 1, 2),
+(6, 'Americano', 15000, 'produk1766115233.jpg', '<p>kopi hitam pahit tanpa gula yang cocok untuk penderita maag (disarankan minta 10 shot)</p>\r\n', 1, 2),
+(7, 'Latte', 20000, 'produk1766115311.jpg', '<p>coffe latte yang ngelatte banget</p>\r\n', 1, 2),
+(8, 'Slow Drip Coffee', 25000, 'produk1766160336.jpg', '<p>kopi yang disajikan dengan cara yang berbeda, cocok untuk anda yang kesabarannya setipis tisu dibagi 7 (Tapi jika anda orang yang penyabar ini cocok)</p>\r\n', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -59,7 +81,14 @@ CREATE TABLE `user` (
   `namauser` varchar(100) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`iduser`, `namauser`, `username`, `password`) VALUES
+(1, 'fella', 'fella', '827ccb0eea8a706c4c34a16891f84e7b');
 
 --
 -- Indexes for dumped tables
@@ -91,19 +120,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `idkategori` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `idkategori` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `idproduk` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `idproduk` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `iduser` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `iduser` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- ilham/login.php
+++ ilham/login.php
@@ -1,69 +1,63 @@
+<?php
+session_start(); // Letakkan session_start di paling atas
+include 'koneksi.php';
+
+if (isset($_POST['submit'])) {
+    // Mengambil data dari form
+    $user = mysqli_real_escape_string($conn, $_POST['user']);
+    $pass = mysqli_real_escape_string($conn, $_POST['pass']);
+
+    // Cek database
+    $cek = mysqli_query($conn, "SELECT * FROM admin WHERE username ='" . $user . "' AND password = '" . MD5($pass) . "'");
+
+    if (mysqli_num_rows($cek) > 0) {
+        $d = mysqli_fetch_object($cek);
+        $_SESSION['status_login'] = true;
+        $_SESSION['a_global'] = $d;
+        $_SESSION['id'] = $d->idadmin;
+        echo '<script>window.location="dashboard.php"</script>';
+    } else {
+        echo '<script>alert("Username atau Password Anda Salah !!")</script>';
+    }
+}
+?>
 <!DOCTYPE html>
-<html>
+<html lang="id">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Kedai Kito Online</title>
+    <title>Kedai Kito Online - Login</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
-
 </head>
 
 <body>
-    <form action="" method="POST">
-        <div class="container mt-3">
-            <div class="row">
-                <div class="col">
-
-                </div>
-                <div class="col">
-                    <div class="card">
-                        <div class="card-header">
-                            LOGIN
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <form action="" method="POST">
+                    <div class="card shadow">
+                        <div class="card-header text-center bg-primary text-white">
+                            <strong>LOGIN KEDAI KITO</strong>
                         </div>
                         <div class="card-body">
                             <div class="mb-3">
-                                <labelclass="username">Username</label>
-                                    <input type="username" class="form-control" name="user" placeholder="Username">
+                                <label class="form-label">Username</label>
+                                <input type="text" class="form-control" name="user" placeholder="Username" required>
                             </div>
                             <div class="mb-3">
-                                <label for="password" class="form-label">Password</label>
-                                <input type="password" id="inputPassword5" class="form-control" name="pass" placeholder="Password">
+                                <label class="form-label">Password</label>
+                                <input type="password" class="form-control" name="pass" placeholder="Password" required>
                             </div>
                         </div>
-                        <div class="card-footer">
+                        <div class="card-footer d-grid">
                             <button type="submit" name="submit" class="btn btn-primary">Login</button>
                         </div>
                     </div>
-                </div>
-                <div class="col">
-
-                </div>
+                </form>
             </div>
         </div>
-    </form>
-    <?php
-    if (isset($_POST['submit'])) {
-        session_start();
-        include 'koneksi.php';
-
-        $user    = mysqli_real_escape_string($conn, $_POST['user']);
-        $pass    = mysqli_real_escape_string($conn, $_POST['pass']);
-
-        $cek = mysqli_query($conn, "SELECT * FROM admin WHERE username ='" . $user . "' AND password = '" . MD5($pass) . "'");
-
-        if (mysqli_num_rows($cek) > 0) {
-            $d = mysqli_fetch_object($cek);
-            $_SESSION['status_login'] = true;
-            $_SESSION['a_global'] = $d;
-            $_SESSION['id'] = $d->idadmin;
-            echo '<script>window.location="dashboard.php"</script>';
-        } else {
-            echo '<script>alert("Username atau Password Anda Salah !!")</script>';
-        }
-    }
-
-    ?>
+    </div>
 </body>
 
 </html>
